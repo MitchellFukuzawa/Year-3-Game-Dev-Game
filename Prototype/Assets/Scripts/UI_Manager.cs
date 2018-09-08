4080796a4338f15e069f4e4a6e69af9b186a59e4
Mitchell Fukuzawa <mitchellfukuzawa@hotmail.com>
2018-09-07 22:09:09 -0400

    Main Menu + Gun Script

---
+++ Prototype/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour {
+	
+	public void Play()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
