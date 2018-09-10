7dc45a39b4b48d2db1bd1452e5571e1861a25741
JonWaaler <jonwaaler@outlook.com>
2018-09-09 23:04:21 -0400

    Death basic
    OnCollision  with a game object tagged "Bullet" the player will be destroyed. NOTE: Need controllers and 2 player update for futher testing.
    
    Going to see if i can squeeze anything else into the code

---
+++ Prototype/Assets/Scripts/WinDetection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinDetection : MonoBehaviour {
+
+    // Attach to the player.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if this player collides with the bullet
+        if(other.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
