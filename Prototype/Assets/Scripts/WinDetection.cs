d35911e34c77023fc8568b8d656ef986060adc9d
JonWaaler <jonwaaler@outlook.com>
2018-09-09 23:32:56 -0400

    player specific tags
    Add 2 player specific tags to make the bullet tag obsolete

--- Prototype/Assets/Scripts/WinDetection.cs
+++ Prototype/Assets/Scripts/WinDetection.cs
@@ -8,11 +8,19 @@ public class WinDetection : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
+
+        if(other.gameObject.tag == "Player_1 Bullet") // This tells us whoes bullet killed this gameobject
+        {
+            // Add point or take away life from this player (aka gameobject)
+            // Then destroy the player, or destory and respawn the player
+
+            
+        }
+
         // if this player collides with the bullet
         if(other.gameObject.tag == "Bullet")
         {
             Destroy(gameObject);
         }
     }
-
 }
