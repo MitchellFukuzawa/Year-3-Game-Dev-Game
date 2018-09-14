222b1802070313597f70d8884ae9f708a86784a9
JonWaaler <jonwaaler@outlook.com>
2018-09-14 16:06:54 -0400

    Networking start... Local hosting on pc tested, can no longer see players on other client but both 2 players are controlable from the main unity window. To use: cntl + B to build window               Play on unity window and click host                    local play on extern window                Enable both spawned players as they are disabled when they spawn                   on unity window play with both conrtollers

---
+++ Prototype/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class MyNetworkManager : NetworkManager
+{
+    private int i = 1;
+    public Transform pos1;
+    public Transform pos2;
+
+
+    public override void OnClientConnect(NetworkConnection conn) {
+         ClientScene.AddPlayer(conn, 0);
+     }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+
+        print("Spawning...");
+        GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        switch (i)
+        {
+            case 1:
+                player.transform.position = pos1.position;
+                print("Spawning Pos1");
+                break;
+            case 2:
+                player.transform.position = pos2.position;
+                print("Spawning Pos2");
+                break;
+        }
+        //player.GetComponent<Player>().color = Color.red;
+        print("Spawn Object Name:" + player.name);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        player.name = player.name + i;
+        player.GetComponent<Player>().H_LS_PNum = "H_LStick" + i.ToString();
+        player.GetComponent<Player>().V_LS_PNum = "V_LStick" + i.ToString();
+        player.GetComponent<Player>().H_RS_PNum = "H_RStick" + i.ToString();
+        player.GetComponent<Player>().V_RS_PNum = "V_RStick" + i.ToString();
+        player.transform.GetChild(1).GetComponent<GunBehavior>().RT_PNum = "RT" + i.ToString();
+        player.transform.GetChild(1).GetComponent<GunBehavior>().xButton_PNum = "XButton" + i.ToString();
+
+
+        i++;
+    }
+}
