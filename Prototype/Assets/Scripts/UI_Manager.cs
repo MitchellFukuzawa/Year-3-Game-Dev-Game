374761c2b2b0d4171fb515dd1b1ed4a141cc8c49
Mitchell Fukuzawa <mitchellfukuzawa@hotmail.com>
2018-09-19 19:58:53 -0400

    Player Selection Menu

--- Prototype/Assets/Scripts/UI_Manager.cs
+++ Prototype/Assets/Scripts/UI_Manager.cs
@@ -5,6 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour {
 	
+	public GameObject mainmenu;
+	public GameObject characterselect;
+	public CanvasGroup PressA;
+	
+	private float fade = 1f;
+	private bool Fade = true;
+
+	void Awake()
+	{
+		mainmenu.SetActive(true);
+		characterselect.SetActive(false);
+	}
+
+	void FixedUpdate()
+	{
+		// Text fade in and out
+		if (fade >= 1f)
+			Fade = true;
+		else if (fade <= 0.25f)
+			Fade = false;
+
+		if (Fade)
+			fade -= Time.deltaTime;
+		else
+			fade += Time.deltaTime;
+
+		PressA.alpha = fade;
+	}
+
 	public void Play()
 	{
 		SceneManager.LoadScene(1);
